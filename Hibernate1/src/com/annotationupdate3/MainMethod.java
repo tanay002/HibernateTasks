f908f6597e310cb0a51502c40cac7fcbee06a631
Tanay Saxena <tanay.saxena002@gmail.com>
2020-02-21 22:08:47 +0530

    Hibernate Pro

---
+++ Hibernate1/src/com/annotationupdate3/MainMethod.java
@@ -0,0 +1,28 @@
+package com.annotationupdate3;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+public class MainMethod 
+{
+	public static void main(String[] args)
+	{
+		Configuration cf=new AnnotationConfiguration();	
+		cf.configure("com/annotationupdate3/comp.cfg.xml");
+		SessionFactory sf=cf.buildSessionFactory();
+		Session sess=  sf.openSession();
+		Employeee e=new Employeee();
+		int id=2;
+		e.setEname("chacha");
+		e.setEadd("bhopal");
+		e.setEid(2);
+		Transaction tx= sess.beginTransaction();
+		sess.update(e);
+		tx.commit();
+		sess.close();
+		System.out.println("Done....!");
+	}
+}
